7561bb006c43de6155becdbea9475effd809681e
KennedyProject <abdulmutaqin14@gmail.com>
2021-07-07 15:59:06 +0700

    Update and rename geezhelper.py to kenhelper.py

--- userbot/modules/kenhelper.py
+++ userbot/modules/kenhelper.py
@@ -8,26 +8,26 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
 
-@register(outgoing=True, pattern="^.lhelp$")
+@register(outgoing=True, pattern="^.khelp$")
 async def usit(e):
     await e.edit(
         f"**Halo {DEFAULTUSER} Jika Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
-        "\n[Telegram](t.me/VckyouuBitch)"
-        "\n[Repo](https://github.com/Vckyou/Geez-UserBot)"
-        "\n[Instagram](instagram.com/Vckyouuu)")
+        "\n[Telegram](t.me/xgothboi)"
+        "\n[Repo](https://github.com/KennedyProject/KEN-UBOT)"
+        "\n[Instagram](instagram.com/acxken._)")
 
 
 @register(outgoing=True, pattern="^.vars$")
 async def var(m):
     await m.edit(
         f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
-        "\n[DAFTAR VARS](https://raw.githubusercontent.com/vckyou/Geez-UserBot/Geez-UserBot/varshelper.txt)")
+        "\n[DAFTAR VARS](https://raw.githubusercontent.com/KennedyProject/KEN-UBOT/KEN-UBOT/varshelper.txt)")
 
 
 CMD_HELP.update({
     "helper":
-    "`.lhelp`\
-\nUsage: Bantuan Untuk Geez-Project.\
+    "`.khelp`\
+\nUsage: Bantuan Untuk KEN-UBOT.\
 \n`.vars`\
 \nUsage: Melihat Daftar Vars."
 })
