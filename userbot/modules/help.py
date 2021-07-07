af275750b56fd1a7a36a3df1d2461568448a2e6e
KennedyProject <abdulmutaqin14@gmail.com>
2021-07-07 23:24:29 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -32,8 +32,8 @@ async def help(event):
         string = ""
         for i in CMD_HELP:
             string += "`" + str(i)
-            string += "`\t ⊖  "
-        await event.edit("**⚡Geez - Project⚡**\n\n"
+            string += "`\t  𓅈  "
+        await event.edit("**⭐KEN-UBOT⭐**\n\n"
                          f"**◉ Bᴏᴛ ᴏꜰ {DEFAULTUSER}**\n**◉ Mᴏᴅᴜʟᴇꜱ : {len(modules)}**\n\n"
                          "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
                          f"◉ {string}◉\n\n")
